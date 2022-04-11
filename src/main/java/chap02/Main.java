e28c1e52018bd1c9a6c4d8ec8606c5a7b3e8a8e9
KyuCheol Hwang <97iambigman@gmail.com>
2022-04-11 17:31:51 +0900

    메인 함수 수정

--- src/main/java/chap02/Main.java
+++ src/main/java/chap02/Main.java
@@ -8,13 +8,16 @@ public class Main {
 		
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
 		
+		//빈 객체 가져오기
 		Sports sports = ctx.getBean("sports", Sports.class);
 		Game game = ctx.getBean("game", Game.class);
 		Team team1 = ctx.getBean("team1", Team.class);
 		
+		//setName
 		sports.setName("스포츠 이름 입니다.");
 		game.setName("게임 이름 입니다.");
 		
+		//빈 객체 정보 출력
 		sports.info();
 		game.info();
 		team1.info();
